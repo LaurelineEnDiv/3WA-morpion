20f390745acf336edffabf65429cf0144827e101
LaurelineEnDiv <laureline.auger@gmail.com>
2022-12-19 15:15:34 +0000

    fonction win

--- script.js
+++ script.js
@@ -13,7 +13,18 @@ document.addEventListener("DOMContentLoaded", () => {
     let player = document.getElementById("player")
     const again = document.querySelector("button")
 
-    const game = ["", "", ""]
+    
+    let win = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [3,4,6]
+        ]
+    
     let nbClick = 0
 
     for (let i = 0; i < box.length; i++) {
@@ -41,6 +52,38 @@ document.addEventListener("DOMContentLoaded", () => {
             element.innerText = " "
         })
     });
+
+    // fonction jeu gagnant
+
+    for (let i = 0 ; i<win.length; i++){
+        let game = ["", "", ""]
+        
+    if(box[win[i][0]].innerText==="X") {
+        game[0]=true
+    }    
+    
+    else if (box[win[i][0]].innerText==="O") {
+        game[0]=false
+    }   
+    
+    if(box[win[i][1]].innerText==="X") {
+        game[1]=true
+    }    
+    
+    else if (box[win[i][1]].innerText==="O") {
+        game[1]=false
+    }  
+    
+    if(box[win[i][2]].innerText==="X") {
+        game[2]=true
+    }    
+    
+    else if (box[win[i][2]].innerText==="O") {
+        game[2]=false
+    }   
+        
+    }
+    
 
 
 })
