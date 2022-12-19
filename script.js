9fa4466c74a991dc69b18314c52b4c3a4bc6c267
LaurelineEnDiv <laureline.auger@gmail.com>
2022-12-19 10:35:58 +0000

    init

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     
+    const plateau = document.getElementsByClassName("container")
+    const box[] = document.querySelectorAll(".container>div")
+    let player = document.getElementById("player")
+    const again = document.querySelector("button")
     
+    const game = ["", "", ""]
+    let nbClick = 0
     
-})
+    for (let i = 0, i<box.length; i++) {
+        
+        box[i].addEventListener("click", () => {
+            
+            if (nbClick % 2 === 1) {
+                
+            }
+            
+            else {
+                
+            }
+        })
+        
+    }
+    
+}
+    
