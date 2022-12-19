144b88370effef2db1f4c1dc21226f1e720bfff8
LaurelineEnDiv <laureline.auger@gmail.com>
2022-12-19 11:23:42 +0000

    init

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
+
+// DOM Element
+// on Click
+// ["", "", ""] notre jeu
+// cas de victoire [[0,1,2,3][0,1,2,3]...]
+// btn restore
+// function jeu gagnant
+// function changer de joueur
+
+
 document.addEventListener("DOMContentLoaded", ()=> {
     
-    const plateau = document.getElementsByClassName("container")
-    const box[] = document.querySelectorAll(".container>div")
+    const box = document.querySelectorAll(".container>div")
     let player = document.getElementById("player")
     const again = document.querySelector("button")
     
     const game = ["", "", ""]
     let nbClick = 0
     
-    for (let i = 0, i<box.length; i++) {
+    for (let i = 0; i<box.length; i++) {
         
         box[i].addEventListener("click", () => {
-            
+            nbClick++;
             if (nbClick % 2 === 1) {
+                player.innerText = "O, à vous de jouer"
+                box[i].innerText = "O"
                 
             }
             
             else {
-                
+                player.innerText = "X, à vous de jouer"
+                box[i].innerText = "X"
             }
         })
         
     }
     
-}
+})
     
